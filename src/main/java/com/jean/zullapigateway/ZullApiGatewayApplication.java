11517c849e26d2fe2ab272d66de84ae53d18789e
jean <jean.leal22@gmail.com>
2020-02-14 23:06:24 -0300

    initial commit

---
+++ src/main/java/com/jean/zullapigateway/ZullApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jean.zullapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZullApiGatewayApplication.class, args);
+    }
+
+}
