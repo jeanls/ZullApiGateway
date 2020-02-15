0873d3816454262ee7e235d464d5f3e51f471976
jean <jean.leal22@gmail.com>
2020-02-14 23:33:34 -0300

    start server

--- src/main/java/com/jean/zuulapigateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/jean/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.jean.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ZuulApiGatewayApplication {
 
     public static void main(String[] args) {
