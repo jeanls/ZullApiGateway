4b885593babbe8849d3bd57bb2f2f17a445d5b69
jean <jean.leal22@gmail.com>
2020-02-16 11:30:31 -0300

    security for api gateway

---
+++ src/main/java/com/jean/zuulapigateway/security/AuthorizationFilter.java
@@ -0,0 +1,53 @@
+package com.jean.zuulapigateway.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+    Environment environment;
+
+    public AuthorizationFilter(AuthenticationManager manager, Environment environment) {
+        super(manager);
+        this.environment = environment;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String authHeader = request.getHeader(environment.getProperty("auth.token.header.name"));
+        if (authHeader == null || !authHeader.startsWith(environment.getProperty("auth.token.header.prefix"))) {
+            chain.doFilter(request, response);
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String authHeader = request.getHeader(environment.getProperty("auth.token.header.name"));
+        if (authHeader == null) {
+            return null;
+        }
+        String token = authHeader.replace(environment.getProperty("auth.token.header.prefix"), "");
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("auth.token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+        if (userId == null) {
+            return null;
+        }
+        return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+    }
+}
--- src/main/java/com/jean/zuulapigateway/security/WebSecurity.java
+++ src/main/java/com/jean/zuulapigateway/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.jean.zuulapigateway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +24,14 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new AuthorizationFilter(authenticationManager(), environment));
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
